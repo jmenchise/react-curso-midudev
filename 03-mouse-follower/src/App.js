2a11cb05781e1393ca52a9c50b7e0c0615140e00
Joan Menchise <jmenchise@gmail.com>
2023-06-23 20:45:32 -0300

    Mouse Follower terminado

---
+++ 03-mouse-follower/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import FollowMouse from './components/FollowMouse';
+
+
+
+export default function App() {
+
+  return (
+    <div>
+      <FollowMouse />
+    </div>
+  );
+}
+
