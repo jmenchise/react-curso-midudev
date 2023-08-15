50e486ed507a0106c8a86e20b49c8a08a016a57f
Joan Pablo David MENCHISE <jmenchise@edenor.com>
2023-08-15 14:32:14 -0300

    primera parte del reducer y definición de estado inicial

--- 09-google-translate-clone/src/App.tsx
+++ 09-google-translate-clone/src/App.tsx
@@ -1,8 +1,54 @@
-import './App.css';
+/* eslint-disable @typescript-eslint/explicit-function-return-type */
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { useReducer } from 'react';
 
-function App(): JSX.Element {
+const initialState = {
+   fromLanguage: 'auto',
+   toLanguage: 'en',
+   fromText: '',
+   resultText: '',
+   loading: false
+};
+
+function reducer(state, { type, payload }) {
+   switch (type) {
+      case 'INTERCHANGE_LANGUAGES':
+         return {
+            ...state,
+            fromLanguage: state.toLanguage,
+            toLanguage: state.fromLanguage
+         };
+
+      case 'SET_FROM_LANGUAGE':
+         return {
+            ...state,
+            fromLanguage: payload
+         };
+
+      case 'SET_TO_LANGUAGE':
+         return {
+            ...state,
+            toLanguage: payload
+         };
+
+      case 'SET_FROM_TEXT':
+         return {
+            ...state,
+            fromText: payload
+         };
+
+      default:
+         return state;
+   }
+}
+
+function App() {
+   const [state, dispatch] = useReducer(reducer, initialState);
+
    return (
-      <h1>Google Translate Clone</h1>
+      <div className="App">
+         <h1>Google Translate Clone</h1>
+      </div>
    );
 }
 
