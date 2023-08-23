8122d6f0e6fac885589648519ac65930ac41f83b
Joan Menchise <jmenchise@gmail.com>
2023-08-22 22:04:10 -0300

    CRUD primera parte. creación de proyecto y renderizado de la lista de usuarios con estilos básicos. se instaló redux toolkit

---
+++ 10-crud-react-redux/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css';
+import { UserList } from './components/UsersList';
+
+// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+function App() {
+   return (
+      <>
+         <UserList />
+      </>
+   );
+}
+
+export default App;
