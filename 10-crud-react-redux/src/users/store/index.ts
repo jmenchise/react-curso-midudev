aee6d05fdd8b1e7c67ff998633c808916cfd34d0
Joan Menchise <jmenchise@gmail.com>
2023-08-28 19:31:41 -0300

    actualización del estado Completa. falta configurar middleware de act en DB y rollback del state

--- 10-crud-react-redux/src/users/store/index.ts
+++ 10-crud-react-redux/src/users/store/index.ts
@@ -1,35 +1,31 @@
 import { type Middleware, configureStore } from '@reduxjs/toolkit';
 import usersReducer from './slice';
-// import { toast } from 'sonner';
 
 const persistanceLocalStorageMiddleware: Middleware = (store) => (next) => (action) => {
    next(action);
    localStorage.setItem('redux-state', JSON.stringify(store.getState()));
 };
 
-// const syncWithDatabase: Middleware = () => (next) => (action) => {
-//    const { type, payload } = action;
-//    console.log('action:', action);
-//    next(action);
-//    // const user = JSON.stringify(payload);
-//    // console.log('user:', user);
-//    console.log('payload:', payload);
-//    if (type === 'users/addNewUser') {
-//       fetch('http://127.0.0.1:8080', {
-//          method: 'POST',
-//          body: payload,
-//          headers: {
-//             'content-type': 'application/json'
-//          }
-//       }).then(res => {
-//          if (res.ok) {
-//             toast.success('Usuario Guardado Correctamente!');
-//          }
-//       }).catch(error => {
-//          console.log(error);
-//       });
-//    }
-// };
+//* Este middleware es para realizar el fetch de datos con la DB.
+/* const syncWithDatabase: Middleware = () => (next) => (action) => {
+   const { type, payload } = action;
+   next(action);
+   if (type === 'users/addNewUser') {
+      fetch('http://127.0.0.1:8080', {
+         method: 'POST',
+         body: JSON.stringify(payload),
+         headers: {
+            'content-type': 'application/json'
+         }
+      }).then(res => {
+         if (res.ok) {
+            toast.success('Usuario Guardado Correctamente!');
+         }
+      }).catch(error => {
+         console.log(error);
+      });
+   }
+}; */
 
 export const store = configureStore({
    reducer: {
